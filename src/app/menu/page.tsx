06b2b30ac8b3ee005bf31284c55609e25c5c48b5
shivaram19 <shivaramiitmb085@gmail.com>
2023-11-10 12:21:36 +0530

    added navbar ,mobile reponsive

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MenuPage = () => {
+  return (
+    <div>
+      MenUpAGE
+    </div>
+  )
+}
+
+export default MenuPage
---
+++ src/components/CartIcon.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const CartIcon = () => {
+  return (
+    <Link href="/cart"  className="flex items-center justify-center gap-2">
+      <div className="relative w-8 h-8">
+        <Image alt='' src="/cart.png" fill/>
+      </div>
+      <span>CART (3)</span>
+    </Link>
+  )
+}
+
+export default CartIcon
